fba75ebcda166e7140d47107c202446901798645
Alexa Buko <asbukowski@stu.naperville203.org>
2017-04-26 18:30:57 -0500

    Sets up classes for the monsters

---
+++ FinalProjectCode/BossMonster.java
@@ -0,0 +1,46 @@
+
+/**
+ * Write a description of class BossMonster here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BossMonster extends Monster
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class BossMonster
+     */
+    public BossMonster()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * 
+     * This will make it so when a boss monster dies it splits up into 4 normal monsters
+     */
+    public void split(int normalmonsterhealth, int normalmonsterspeed)
+    {
+        
+        
+        
+    }
+    
+    
+    /**
+     * 
+     * This makes the boss mob move
+     */
+    public void move()
+    {
+        
+        
+        
+    }
+    
+    
+}
