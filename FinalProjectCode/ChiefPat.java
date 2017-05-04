ff61a477830ebb7231e21ef14564d98cde0d596c
daveyoshi <DTBukowski@student.naperville203.org>
2017-05-04 15:10:38 -0500

    Cheif Pat setup

---
+++ FinalProjectCode/ChiefPat.java
@@ -0,0 +1,26 @@
+
+
+/**
+ * Write a description of class ChiefPat here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ChiefPat implements Hero
+{
+   
+    
+    private int dx;
+    private int dy;
+    private int x;
+    private int y;
+    
+
+    public ChiefPat()
+    {
+        
+    }
+
+   
+
+}
