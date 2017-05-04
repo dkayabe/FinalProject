b67baf42fd578e1dc6f3203505ffa4ac6d7c2852
dkayabe <danlka1903@gmail.com>
2017-05-03 22:02:58 -0500

    5/3/17
    remade the entire project. Made TransparentPNG, and messed with its
    transparentness.

---
+++ FinalProjectCode/InfernoRifleBestie.java
@@ -0,0 +1,68 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+import java.util.*;
+/**
+ * Write a description of class InfernoRifleBestie here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class InfernoRifleBestie
+{
+    // instance variables - replace the example below with your own
+    Graphics2D g2;
+    
+    private int dx;
+    private int dy;
+    private int x;
+    private int y;
+    private Image image;
+    
+    private ImageIcon hero;
+    /**
+     * Constructor for objects of class InfernoRifleBestie
+     */
+    public InfernoRifleBestie()
+    {
+        hero = new ImageIcon("inferno_rifle_bestie.gif");
+        x = 40;
+        y = 60;
+    }
+    
+    public void paintComponent(Graphics g) {
+        g2 = (Graphics2D) g;
+        BufferedImage imgj = null;
+        try {
+            URL url = new URL("inferno_rifle_bestie.gif");
+            img = img = ImageIO.read(url);
+        }
+        catch (Exception e) {
+            
+        }
+        g2.drawImage(hero5, 5, this);
+    }
+    
+    public void move() {
+        x += dx;
+        y += dy;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+}
+
---
+++ FinalProjectCode/TransparentPNG.java
@@ -0,0 +1,80 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.LinearGradientPaint;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class TransparentPNG {
+
+    public static void main(String[] args) {
+        new TransparentPNG();
+    }
+
+    public TransparentPNG() {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                }
+
+                JFrame frame = new JFrame("Testing");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLayout(new BorderLayout());
+                frame.add(new TestPane());
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+        });
+    }
+
+    public class TestPane extends JPanel {
+
+        private BufferedImage img;
+
+        public TestPane() {
+            try {
+                img = ImageIO.read(new File("BraveFrontierBestie.png"));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return img == null ? new Dimension(200, 200) : new Dimension(img.getWidth(), img.getHeight());
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g.create();
+            LinearGradientPaint lgp = new LinearGradientPaint(
+                    new Point(0, 0),
+                    new Point(0, getHeight()),
+                    new float[]{0f, 1f},
+                    new Color[]{Color.GREEN, Color.YELLOW});
+            g2d.setPaint(lgp);
+            g2d.fillRect(0, 0, getWidth(), getHeight());
+            if (img != null) {
+                int x = (getWidth() - img.getWidth()) / 2;
+                int y = (getHeight() - img.getHeight()) / 2;
+                g2d.drawImage(img, x, y, this);
+            }
+            g2d.dispose();
+        }
+    }
+}
