8f57a769a334ec072bda94167ee00804bfc07097
Taher-Dohadwala <TKDohadwala@student.naperville203.org>
2017-05-04 15:10:37 -0500

    working on hero interface

--- FinalProjectCode/Hero.java
+++ FinalProjectCode/Hero.java
@@ -1,17 +1,11 @@
 
-/**
- * Write a description of interface Hero here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public interface Hero
 {
-    /**
-     * An example of a method header - replace this comment with your own
-     * 
-     * @param  y    a sample parameter for a method
-     * @return        the result produced by sampleMethod 
-     */
-    int sampleMethod(int y);
+    public void show_image();
+    
+    public void setHealth();
+    
+    public void showHealthBar();
+    
+    public void move();
 }
