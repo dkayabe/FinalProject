8b7b4b55415a8f7548e73e053d1f670b3cffc22c
dkayabe <DKAyabe@student.naperville203.org>
2017-05-04 15:10:15 -0500

    5/4/17
    bla

--- FinalProjectCode/InfernoRifleBestie.java
+++ FinalProjectCode/InfernoRifleBestie.java
@@ -1,3 +1,4 @@
+/*
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -6,13 +7,18 @@ import java.awt.Image;
 import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
 import java.util.*;
+*/
+import java.awt.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 /**
  * Write a description of class InfernoRifleBestie here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class InfernoRifleBestie
+public class InfernoRifleBestie extends JPanel
 {
     // instance variables - replace the example below with your own
     Graphics2D g2;
@@ -22,6 +28,7 @@ public class InfernoRifleBestie
     private int x;
     private int y;
     private Image image;
+    private BufferedImage img;
     
     private ImageIcon hero;
     /**
@@ -29,14 +36,14 @@ public class InfernoRifleBestie
      */
     public InfernoRifleBestie()
     {
-        hero = new ImageIcon("inferno_rifle_bestie.gif");
+        hero = new ImageIcon("InfernoRifleBestie.PNG");
         x = 40;
         y = 60;
     }
     
     public void paintComponent(Graphics g) {
         g2 = (Graphics2D) g;
-        BufferedImage img = null;
+        
         try {
             URL url = new URL("inferno_rifle_bestie.gif");
             img = img = ImageIO.read(url);
