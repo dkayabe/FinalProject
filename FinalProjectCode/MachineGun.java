86fd37d1821a8494e2c344d1e3d38bdff9fbc422
dkayabe <danlka1903@gmail.com>
2017-05-09 19:27:14 -0500

    5/9/17
    finished machine gun

--- FinalProjectCode/MachineGun.java
+++ FinalProjectCode/MachineGun.java
@@ -1,6 +1,10 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Line2D;
+import java.awt.Color;
+import java.awt.*;
+
+import java.io.*;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,7 +23,13 @@ public class MachineGun extends JComponent
 
     Graphics2D g2;
     
-    private Line2D[] bullets = new Line2D[5];
+    Line2D.Double b1;
+    Line2D.Double b2;
+    Line2D.Double b3;
+    Line2D.Double b4;
+    Line2D.Double b5;
+    
+    private Line2D[] bullets = {b1, b2, b3, b4, b5};
     /**
      * Default constructor for objects of class MachineGun
      */
@@ -29,16 +39,44 @@ public class MachineGun extends JComponent
         this.x = x;
         this.y = y;
         
-        Line2D.Double b1 = new Line2D.Double(newX, y, x, y);
-        Line2D.Double b2 = new Line2D.Double(newX, y, x, y);
-        Line2D.Double b3 = new Line2D.Double(newX, y, x, y);
-        Line2D.Double b4 = new Line2D.Double(newX, y, x, y);
-        Line2D.Double b5 = new Line2D.Double(newX, y, x, y);
+        
+        Line2D.Double b1 = new Line2D.Double(x - 10, y, x, y);
+        Line2D.Double b2 = new Line2D.Double(x - 10, y, x, y);
+        Line2D.Double b3 = new Line2D.Double(x - 10, y, x, y);
+        Line2D.Double b4 = new Line2D.Double(x - 10, y, x, y);
+        Line2D.Double b5 = new Line2D.Double(x - 10, y, x, y);
         
         
-        bullets = {b1, b2, b3, b4, b5};
+        
     }
 
     
+    public void paintComponent(Graphics g){
+        g2 = (Graphics2D) g;
+        g2.setColor(Color.YELLOW);
+        
+        for (Line2D line:bullets) {
+            g2.draw(line);
+            for (int i = 0; i < 30; i++) {
+                x -= 5;
+                repaint();
+                try {
+                    Thread.sleep(1);
+                }
+                catch(InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            repaint();
+            try {
+                Thread.sleep(10);
+            }
+            catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+            
+        }
+        repaint();
+    }
 
 }
